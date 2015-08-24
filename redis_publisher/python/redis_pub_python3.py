f61492e8bb688c5d2458a9fec77d7765c22f234b
Ashwani <ashwani4u4888@gmail.com>
2015-08-24 10:13:08 +0530

    Update redis_pub_python3.py

--- redis_publisher/python/redis_pub_python3.py
+++ redis_publisher/python/redis_pub_python3.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import redis
 
@@ -14,5 +14,5 @@ redisClient = redis.StrictRedis(host = hostname, port = redisPort, db = 0)
 
 redisClient.publish(channelName, message)
 
-print 'published test message to channel ['+channelName+']'
+print ('published test message to channel ['+channelName+']')
 
