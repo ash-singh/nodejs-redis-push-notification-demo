ad8abb666ea05e6b99e1714aae47153928ccc4dd
Ashwani Singh <ahswani4u4888@gmail.com>
2015-08-24 10:14:17 +0530

    changed python2 publisher

--- redis_publisher/python/redis_pub_python2.py
+++ redis_publisher/python/redis_pub_python2.py
@@ -8,7 +8,7 @@ channelName = 'redis_data';
 
 hostname = '127.0.0.1'
 
-message = 'This is a test message from python3 redis publisher.'
+message = 'This is a test message from python2 redis publisher.'
 
 redisClient = redis.StrictRedis(host = hostname, port = redisPort, db = 0)
 
